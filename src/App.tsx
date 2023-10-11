88ee845e179391008f300d7c01abc1b38919fb2c
nkarshiva <shivankar2906@gmail.com>
2023-10-12 04:13:24 +0530

    initial commit with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mentionOptions from './../data.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  //State variables
+  const [inputText, setInputText] = useState("")
+  const [suggestedMentions, setSuggestedMentions] = useState([] as string[]);
+  const [suggestedMention, setSuggestedMention] = useState("");
+
+  //On text change in input field
+  const onTextChange = (e: any) => {
+    setSuggestedMention("")
+    const text = e.target.value;
+    setInputText(text)
+
+    // Detect mentions starting with @ and return an array containing them
+    const mentions = text.match(/@(\w+)/g);
+    if (mentions) {
+
+      //Getting the substring from the text excluding "@"
+      const mentionUserInput = mentions.map((mention: any) => mention.substring(1));
+     
+      //Filter out the options to show in the dropdown based on character match
+      const filteredMentions = mentionOptions.filter((mention) =>
+        mention.first_name.toLowerCase().startsWith(mentionUserInput[mentionUserInput.length - 1].toLowerCase())
+      );
+
+      //Getting the list of all suggested names in array format
+      const filterMentionNames = filteredMentions.map(mention => mention.first_name + " " + mention.last_name)
+      setSuggestedMentions(filterMentionNames);
+    } else {
+      setSuggestedMentions([]);
+    }
+  }
+
+  //On mention select replace text with mentioned text
+  const handleMentionSelect = (mention: string) => {
+    const mentionText = `@${mention}`;
+    setInputText(inputText.replace(/@(\w+)/g, mentionText));
+    setSuggestedMentions([]);
+    setSuggestedMention(mention)
+  };
+
+  //JSX
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <input className='textBox' placeholder='Mention' value={inputText} onChange={onTextChange} />
+        <div>
+          {(suggestedMentions.length > 0 && suggestedMention==="")&& (
+            <ul className='suggestionBox'>
+              {suggestedMentions.map((mention, index) => (
+                <li key={index} className='suggestion' onClick={() => handleMentionSelect(mention)}>
+                  {mention}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
